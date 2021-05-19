7100c838542734d337dd8c6238bbd6ed608c1b31
Tudor <tudor@blackbullion.com>
2021-05-19 10:54:16 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,38 @@
+const core = require('@actions/core')
+const fs = require('fs').promises
+
+const createNewTag = (previousTag) => {
+  const splitLatestTag = previousTag.split('.')
+  const month = splitLatestTag[1]
+  const release = splitLatestTag[2]
+
+  const newMonth = new Date().getMonth() + 1
+
+  return `${new Date().getFullYear()}.${newMonth}.${newMonth === Number(month) ? Number(release) + 1 : 1}`
+}
+
+const main = async () => {
+  try {
+    let changelogContents = await fs.readFile(core.getInput('filePath'), 'utf8')
+    const unreleasedHeader = 'Unreleased'
+
+    const previousTag = changelogContents
+      .split('\n')
+      .filter((l) => Boolean(l))
+      .find((l) => l.startsWith('## 20')) // works until the 22nd century
+
+    const latestTag = createNewTag(previousTag)
+    changelogContents = changelogContents.replace(unreleasedHeader, latestTag)
+
+    await fs.writeFile(core.getInput('filePath'), changelogContents)
+
+    core.setOutput('newTag', latestTag)
+
+    const changelogForNewRelease = changelogContents.substring(0, changelogContents.indexOf(previousTag))
+    core.setOutput('changelog', changelogForNewRelease)
+  } catch (error) {
+    core.setFailed(error.message)
+  }
+}
+
+main()
