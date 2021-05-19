638d6e26ba1414820f862a95a8fd343bdd4192a0
Tudor <tudor@blackbullion.com>
2021-05-19 14:36:43 +0100

    strip commit url from changelog output

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const core = require('@actions/core')
+const github = require('@actions/github')
 const fs = require('fs').promises
 
 const createNewTag = (previousTag) => {
@@ -13,10 +14,63 @@ const createNewTag = (previousTag) => {
   return `${new Date().getFullYear()}.${newMonth}.${newMonth === Number(month) ? Number(release) + 1 : 1}`
 }
 
+const getUnreleasedChanges = (changelogContents, previousTag) => {
+  const end = previousTag
+    ? changelogContents.indexOf(previousTag)
+    : changelogContents.length
+
+  return changelogContents
+    .substring(0, end)
+    .replace('## Unreleased', '')
+}
+
+const getChangelogCommitsData = (unreleasedContent) => {
+  return unreleasedContent
+    .split('\n')
+    .filter((l) => Boolean(l))
+    .map((line) => {
+      const url = line.split('[commit]')[1].replace(/[()]/g, '')
+
+      return {
+        message: line.split(' ([commit]')[0],
+        sha: url.split('commit/')[1]
+      }
+    })
+}
+
+const formatCommits = (commitsData, latestTag, released) => {
+  const commits =  commitsData.filter((commit) => commit.released === released)
+
+  if (commits.length > 0) {
+    const formattedCommits = commits
+      .map((commit) => `* ${commit.message} ([commit](${commit.sha}))`).
+      join('\n')
+
+    const header = released ? latestTag : 'Unreleased'
+
+    return `## ${header}\n${formattedCommits}\n`
+  } else {
+    return ''
+  }
+}
+
+const formatChangelog = (commitsData) => {
+  const commits =  commitsData.filter((commit) => commit.released === released)
+  
+  if (commits.length > 0) {
+    return commits.map((commit) => `• ${commit.message}`).join('\n')
+  } else {
+    return ''
+  }
+}
+
 const main = async () => {
   try {
-    let changelogContents = await fs.readFile(core.getInput('filePath'), 'utf8')
-    const unreleasedHeader = 'Unreleased'
+    const token = core.getInput('token')
+    const octokit = github.getOctokit(token)  
+    const repo = github.context.repo
+
+    const changelogContents = await fs.readFile(core.getInput('filePath'), 'utf8')
 
     const previousTag = changelogContents
       .split('\n')
@@ -24,12 +78,32 @@ const main = async () => {
       .find((l) => l.startsWith('## 20')) // works until the 22nd century
 
     const latestTag = createNewTag(previousTag)
+
+    // see what commits went into the comparison branch
+    const comparisonBranchCommits = await octokit.rest.repos.listCommits({
+      ...repo,
+      sha: core.getInput('comparisonBranch'),
+      per_page: commitsData.length
+    })
+
+    comparisonBranchSHAs = comparisonBranchCommits.data.map((commit) => commit.sha)
+
+    const unreleasedContent = getUnreleasedChanges(changelogContents, previousTag)
+    const commitsData = getChangelogCommitsData(unreleasedContent).map((commitData) => ({
+      ...commitData,
+      released: comparisonBranchSHAs.includes(commitData.sha)
+    }))
+
+    // only move commits in the comparison branch to the new tag's section
+    await fs.writeFile(core.getInput('filePath'), changelogContents.replace(unreleasedContent, `
+      ${formatCommits(commitsData, latestTag, false)}
+      ${formatCommits(commitsData, latestTag, true)}
+    `))
+
+    const changelogForNewRelease = formatChangelog(commitsData)
+    core.info(changelogForNewRelease)
+
     core.setOutput('newTag', latestTag)
-
-    changelogContents = changelogContents.replace(unreleasedHeader, latestTag)
-    await fs.writeFile(core.getInput('filePath'), changelogContents)
-
-    const changelogForNewRelease = changelogContents.substring(0, previousTag ? changelogContents.indexOf(previousTag) : changelogContents.length)
     core.setOutput('changelog', changelogForNewRelease)
   } catch (error) {
     core.setFailed(error.message)
