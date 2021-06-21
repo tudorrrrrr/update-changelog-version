fc846b47e3e5855ba31ec7519f0558c592bfa54c
Tudor <tudor@blackbullion.com>
2021-06-21 16:36:29 +0100

    cut out git commit descriptions

--- index.js
+++ index.js
@@ -66,6 +66,7 @@ const formatChangelog = (commitsData) => {
 }
 
 const processMessage = (message) => {
+  message = message.split('\n')[0]
   return message.replace(`${prefix}:`, '').trim().toLowerCase()
 }
 
